08cc2d00cfdda045d3e5fcfbcb60e714a772e850
Utkarsh Bothe <utkarsh.22311823@viit.ac.in>
2025-09-08 14:48:22 +0530

    final checkout

--- frontend/chat/src/components/Navbar.jsx
+++ frontend/chat/src/components/Navbar.jsx
@@ -18,25 +18,25 @@ const Navbar = () => {
   const { logoutMutation } = useLogout();
 
   return (
-    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="sticky top-0 z-30 flex items-center h-16 border-b bg-base-200 border-base-300">
+      <div className="container px-4 mx-auto sm:px-6 lg:px-8">
         <div className="flex items-center justify-end w-full">
           {/* LOGO - ONLY IN THE CHAT PAGE */}
           {isChatPage && (
             <div className="pl-5">
               <Link to="/" className="flex items-center gap-2.5">
                 <ShipWheelIcon className="size-9 text-primary" />
-                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-                  Streamify
+                <span className="font-mono text-3xl font-bold tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
+                  Socialize
                 </span>
               </Link>
             </div>
           )}
 
-          <div className="flex items-center gap-3 sm:gap-4 ml-auto">
+          <div className="flex items-center gap-3 ml-auto sm:gap-4">
             <Link to={"/notifications"}>
               <button className="btn btn-ghost btn-circle">
-                <BellIcon className="h-6 w-6 text-base-content opacity-70" />
+                <BellIcon className="w-6 h-6 text-base-content opacity-70" />
               </button>
             </Link>
           </div>
@@ -45,14 +45,14 @@ const Navbar = () => {
           <ThemeSelector />
 
           <div className="avatar">
-            <div className="w-9 rounded-full">
+            <div className="rounded-full w-9">
               <img src={authUser?.profilePic} alt="User Avatar" rel="noreferrer" />
             </div>
           </div>
 
           {/* Logout button */}
           <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
-            <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
+            <LogOutIcon className="w-6 h-6 text-base-content opacity-70" />
           </button>
         </div>
       </div>
